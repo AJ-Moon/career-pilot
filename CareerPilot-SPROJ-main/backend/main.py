edae20c1d6f55f7e4c91d047bde9914b4a1ca7f1
AJ-Moon <146750397+AJ-Moon@users.noreply.github.com>
2025-10-23 22:44:02 +0500

    Update CORS allowed origins for deployment

--- CareerPilot-SPROJ-main/backend/main.py
+++ CareerPilot-SPROJ-main/backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://career-pilot-bzienkku6-ahmad-junaids-projects-b7b12dc4.vercel.app/",],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://career-pilot-gilt.vercel.app/",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
