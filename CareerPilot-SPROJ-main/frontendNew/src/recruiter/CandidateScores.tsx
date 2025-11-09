ff298a9da3ab7f812c3c3f5f95887b69ec100997
AJ-Moon <146750397+AJ-Moon@users.noreply.github.com>
2025-11-09 18:45:23 +0500

    Update CandidateScores.tsx

--- CareerPilot-SPROJ-main/frontendNew/src/recruiter/CandidateScores.tsx
+++ CareerPilot-SPROJ-main/frontendNew/src/recruiter/CandidateScores.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 
-import React from 'react';
+// import React from 'react';
 import { 
   Search, 
   Download, 
